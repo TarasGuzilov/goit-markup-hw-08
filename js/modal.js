35ed2e7d1d0efb8c64f4883d4cee18481172a950
TarasGuzilov <tarasvakcina@rambler.ru>
2021-12-06 21:12:01 +0300

    часть page-header

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]")
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expended") === "true" || false;
+    
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expandend", !expanded);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
